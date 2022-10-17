6cf951f1f7e08821b02aecf2fb51371c19dd67fb
1E10 Laptop <sample@bths.edu>
2022-10-17 11:02:19 -0400

    Initial commit

---
+++ src/LinearEquation.java
@@ -0,0 +1,25 @@
+public class Calculator {
+    /* Instance Variables */
+    private int x1;
+    private int y1;
+    private int x2;
+    private int y2;
+
+
+    public Calculator(int x1, int y1, int x2, int y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
+
+
+    public double distance(){
+        double xDiff = x2-x1;
+        double yDiff = y2-y1;
+        return Math.sqrt((Math.pow(xDiff, 2)) + Math.pow(yDiff,2));
+    }
+
+
+    
+}
