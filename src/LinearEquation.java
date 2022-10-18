be877e7e18dc7f27d99e9b9b16390d99fa0309da
1E10 Laptop <sample@bths.edu>
2022-10-18 11:00:27 -0400

    e

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,3 +1,4 @@
+
 public class LinearEquation {
     /* Instance Variables */
     private int x1;
@@ -11,21 +12,65 @@ public class LinearEquation {
         this.y1 = y1;
         this.x2 = x2;
         this.y2 = y2;
+
     }
 
 
     public double distance(){
         double xDiff = x2-x1;
         double yDiff = y2-y1;
-        return Math.sqrt((Math.pow(xDiff, 2)) + Math.pow(yDiff,2));
+
+        return roundedToHundredth(Math.sqrt((Math.pow(xDiff, 2)) + Math.pow(yDiff,2)));
     }
     public double slope(){
-        double slope = (y2-y1)/(x2-x1);
+        double xDiff = x2-x1;
+        double yDiff = y2-y1;
+        return roundedToHundredth(yDiff/xDiff);
     }
     public double yIntercept(){
-        double slope = (y2-y1)/(x2-x1);
-
+        return roundedToHundredth(y1-(x1*slope()));
     }
+    public String equation() {
+        double xDiff = (x2 - x1);
+        double yDiff = (y2 - y1);
+        if (xDiff < 0){
+            xDiff *= -1;
+            yDiff *= -1;
+        }
+        if (yIntercept() == 0){
+            return "y = " + yDiff/xDiff + "x";
+        }
+        if (slope() == 1){
+            return  "y = " + "x";
+        }
+        if (yIntercept() < 0) {
+            if (yDiff % xDiff == 0){
+                return "y = " + yDiff/xDiff + "x" + " + " + Math.abs(yIntercept());
+            }
+            return "y = " + yDiff + "/" + xDiff + "x" + " - " + Math.abs(yIntercept());
+        }
+        if (yIntercept() > 0){
+            if (yDiff % xDiff == 0){
+                return "y = " + yDiff/xDiff + "x" + " + " + (yIntercept());
+            }
+            return "y = " + yDiff + "/" + xDiff + "x" + " + " + yIntercept();
+        }
+        return "";
+    }
+    public String coordinateForX(double xValue){
+        double y = (xValue*slope() + yIntercept());
+        return "(" +  xValue + ", " + y + ")";
+    }
+    public double roundedToHundredth(double toRound){
+        return (double)(Math.round(toRound * 100))/100;
+    }
+    public  String lineInfo(){
+        return "The two points are: (" + x1 + ", " + y1 + ")" +  " and (" + x2 + ", " + y2 + ")" + "\n"
+                + "The equation of the line between these two points is " + equation() + "\n"
+                + "The slope of the line is " + slope() + "\n" + "The y-intercept of the line is " + yIntercept() + "\n"
+                + "The distance between the two points is " + distance();
+    }
+
 
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Welcome!");
@@ -14,18 +14,17 @@ public class Main {
         int index2 = coord2.indexOf(",");
         int index3 = coord2.indexOf(")");
 
-        int x1 = Integer.parseInt(coord1.substring(1,index));
-        int y1 = Integer.parseInt(coord1.substring(index+2,index1));
-        int x2 = Integer.parseInt(coord2.substring(1,index2));
-        int y2 = Integer.parseInt(coord2.substring(index2+2,index3));
+        int x1 = Integer.parseInt(coord1.substring(1, index));
+        int y1 = Integer.parseInt(coord1.substring(index + 2, index1));
+        int x2 = Integer.parseInt(coord2.substring(1, index2));
+        int y2 = Integer.parseInt(coord2.substring(index2 + 2, index3));
 
-        System.out.println(x1);
-        System.out.println(y1);
-        System.out.println(x2);
-        System.out.println(y2);
 
-        LinearEquation calc = new LinearEquation (x1, y1, x2, y2);
-        System.out.println(calc.distance());
-
+        LinearEquation calc = new LinearEquation(x1, y1, x2, y2);
+        System.out.println(calc.lineInfo());
+        System.out.println("Enter a value for x:");
+        double temp = scan.nextDouble();
+        System.out.println();
+        System.out.println("The point on the line is: " + calc.coordinateForX(temp));
     }
 }
